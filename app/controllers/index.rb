506920142ba16d2773583159e356180685a7d01f
jgerminario <jgerminario@gmail.com>
2015-02-08 15:46:46 -0800

    beginning fix for invalid address data

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,7 +12,7 @@ get '/map' do
   offers.each do |offer|
     full_address = "#{offer.street_address}, San Francisco, CA"
     coordinates = Geocoder.coordinates(full_address)
-    sleep(0.2)
+    sleep(0.4)
     long = coordinates[0]
     lat = coordinates[1]
     new_coordinates = []
--- app/helpers/api_helpers.rb
+++ app/helpers/api_helpers.rb
@@ -6,11 +6,16 @@ helpers do
   def get_zillow_address_data(address, zipcode)
     address = URI.encode(address)
     response = HTTParty.get("http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1az0o6cmnm3_8b748&address=#{address}&citystatezip=#{zipcode}&rentzestimate=true")
-    result = response["searchresults"]["response"]["results"]["result"][0]
-    monthly_market_value = result["rentzestimate"]["amount"]["__content__"]
-    total_market_value = result["zestimate"]["amount"]["__content__"]
-    pp result["bedrooms"]
-    return { monthly_market_value: monthly_market_value.to_i, total_market_value: total_market_value.to_i, bedrooms: result["bedrooms"].to_i}
+    p response["searchresults"]["response"]
+    if response["searchresults"]["response"]
+      result = response["searchresults"]["response"]["results"]["result"][0]
+      monthly_market_value = result["rentzestimate"]["amount"]["__content__"]
+      total_market_value = result["zestimate"]["amount"]["__content__"]
+      pp result["bedrooms"]
+      return { monthly_market_value: monthly_market_value.to_i, total_market_value: total_market_value.to_i, bedrooms: result["bedrooms"].to_i}
+    else
+      return false
+    end
   end
 
   def to_boolean(string)
