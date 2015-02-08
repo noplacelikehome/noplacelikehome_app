ef2e347150e77358d9ea6919ecab2b50b891d2be
Anna Wu <shinshinwu@gmail.com>
2015-02-07 19:37:46 -0800

    starting questions routes

--- app/controllers/offer.rb
+++ app/controllers/offer.rb
@@ -1,7 +1,7 @@
 get '/offer' do
   p monthly_market_value = get_zillow_address_data("725 Leavenworth", "94110")
   args = { offer_price: 25_000,
-           monthly_market_value: monthly_market_value.to_i, 
+           monthly_market_value: monthly_market_value.to_i,
            current_monthly_rent: 2500 } #mmv comes from zillow, cmr & op from user
   calculate_low_offer(args).to_s
 end
@@ -9,5 +9,10 @@ end
 get '/address_data' do
   content_type :json
   get_zillow_address_data("725 Leavenworth", "94110").to_json
+
+end
+
+
+get '/api/get_offer_price' do
 
 end
